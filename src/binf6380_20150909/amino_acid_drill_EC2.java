13cf2c54fae48dea3bfc23ae45cad539a8882d24
reh gar <rehgar12@yahoo.com>
2015-09-12 11:56:10 -0400

    summary report added

---
+++ src/binf6380_20150909/amino_acid_drill_EC2.java
@@ -0,0 +1,112 @@
+package binf6380_20150909;
+
+import java.util.*;		//Daniel Zyska suggested the use of the Scanner util for user input
+
+
+public class amino_acid_drill_EC2
+{
+	private static final Random RANDOM = new Random();
+	
+	public static final String[] SHORT_NAMES = 
+	{
+	"A","R", "N", "D", "C", "Q", "E",
+	"G",  "H", "I", "L", "K", "M", "F",
+	"P", "S", "T", "W", "Y", "V" 
+	};
+
+	public static final String[] FULL_NAMES = 
+	{
+	"alanine","arginine", "asparagine",
+	"aspartic acid", "cysteine",
+	"glutamine",  "glutamic acid",
+	"glycine" ,"histidine","isoleucine",
+	"leucine",  "lysine", "methionine",
+	"phenylalanine", "proline",
+	"serine","threonine","tryptophan",
+	"tyrosine", "valine"
+	};
+	
+	
+	public static void main(String[] args)
+	{
+		//combining short and full name arrays into a hashmap
+		HashMap<String, String> aa_Hash = new HashMap<>();
+//		System.out.println(SHORT_NAMES.length);
+//		System.out.println(");
+		for( int x=0; x<SHORT_NAMES.length; x++)
+		{
+			aa_Hash.put(SHORT_NAMES[x], FULL_NAMES[x]);
+		}	
+		//iterate through hashmap and print each key,value pair
+		Iterator<String> keySetIterator = aa_Hash.keySet().iterator();
+		while(keySetIterator.hasNext())
+		{
+			String key = keySetIterator.next();
+//			System.out.println("key: " + key + " value " + aa_Hash.get(key));
+		}
+
+
+		
+		//hashmap for storing answers
+		HashMap<String, String> answer_Hash = new HashMap<>();
+		
+		//TIMING
+		//user input for timer
+		Scanner user_input_time = new Scanner(System.in);
+		System.out.print("This is a program to drill your knowledge of the single letter codes for the 20 amino acids.\nHow long would you like to practice? (Enter number of seconds) ");
+		long userTime = user_input_time.nextLong();
+		long startTime = System.currentTimeMillis();
+		long waitTime = userTime*1000;
+		long endTime = startTime + waitTime;
+		long elapsedTime;
+		
+		//COUNTERS
+		int rightCount = 0;
+		int wrongCount = 0;
+		int total = rightCount+wrongCount;
+		
+		do
+		{
+			Scanner user_input = new Scanner(System.in);
+			//display a random AA from the array
+			int rand = RANDOM.nextInt(20);
+//			System.out.println(rand);
+			String user_guess = "";
+			
+			//for the random aa printed to screen, get user input
+			System.out.print("What is the single letter code of: "+FULL_NAMES[rand]+"?	");
+			user_guess = user_input.next();
+			String user_guess_upper = user_guess.toUpperCase();
+			
+			
+			if( FULL_NAMES[rand].equals(aa_Hash.get(user_guess_upper)))
+			{
+				rightCount++;
+				total = rightCount+wrongCount;
+				elapsedTime = (System.currentTimeMillis()-startTime)/1000;
+				System.out.println("	Correct! Score="+rightCount+" at "+elapsedTime+" seconds out of "+userTime+".");
+				answer_Hash.put("Question#"+total, "Amino acid prompted:	"+FULL_NAMES[rand]+".\n		You answered:		"+user_guess_upper+".\n		Result:			Correct! Score="+rightCount+" at "+elapsedTime+" seconds out of 30.");
+			}
+			else
+			{
+				//does not exit program on wrong answer, instead gives correct letter
+				//asks 
+				wrongCount++;
+				total = rightCount+wrongCount;
+				System.out.println("	WRONG! The single letter code for "+FULL_NAMES[rand]+" is "+SHORT_NAMES[rand]);
+				answer_Hash.put("Question#"+total, "Amino acid prompted:	"+FULL_NAMES[rand]+".\n		You answered:		"+user_guess_upper+".\n		Result:			WRONG! The single letter code for "+FULL_NAMES[rand]+" is "+SHORT_NAMES[rand]+".");
+			}
+		} while( System.currentTimeMillis()<endTime );
+		System.out.println("\nSUMMARY REPORT\nYou answered "+rightCount+" correct out of "+total+" attempted.");
+		//REPORT
+		//store keys in array to sort
+		Object[] keyArray = answer_Hash.keySet().toArray();
+		Arrays.sort(keyArray);
+		//iterate through answer_Hash by sorted key array
+		for( Object key2 : keyArray )
+		{
+			System.out.println(key2+"	"+answer_Hash.get(key2));
+		}
+	}
+}
+
