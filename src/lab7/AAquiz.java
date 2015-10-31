4c96ebeb26598ed03ae3f58d08c6a434d0f5d3d5
reh gar <rehgar12@yahoo.com>
2015-10-31 10:34:10 -0400

    for win10 eclipse

--- src/lab7/AAquiz.java
+++ src/lab7/AAquiz.java
@@ -1,14 +1,106 @@
 package lab7;
 
-import lab6_part2.*;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 
-public class AAquiz
+public class AAquiz extends JFrame
 {
+	private static final long serialVersionUID = -348528394792123L;
+	
+	private JTextArea instructions = new JTextArea();
+	private JLabel practiceTime = new JLabel();
+	private JTextField enterTime = new JTextField();
+	private JButton startButton = new JButton("Start Quiz");
+	private JButton quitButton = new JButton("Close");
+	
+	private void updateText()
+	{
+		instructions.setText("This is a quiz to drill your knowledge of the single letter codes for the 20 amino acids.\nHow long would you like to practice? (Default is 30 seconds)");
+		instructions.setLineWrap(true);
+		instructions.setEditable(false);
+		
+		practiceTime.setText("Enter number of seconds:");
+		validate();
+	}
+	
+	//action listeners
+	private class textFieldAction implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			Long userTime = Long.parseLong(enterTime.getText());
+		}
+	}
+	private class startActionListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			new quizFunc("Amino Acid Quiz: Goodluck!");
+		}
+	}
+	private class quitActionListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			System.exit(0);
+		}
+	}
+	
+	
+	private JPanel getTimePanel()
+	{
+		JPanel tpanel = new JPanel();
+		tpanel.setLayout(new GridLayout(0,2));
+		tpanel.add(practiceTime);
+		tpanel.add(enterTime);
+		
+		return tpanel;
+	}
+	private JPanel getBottomPanel()
+	{
+		JPanel bpanel = new JPanel();
+		bpanel.setLayout(new GridLayout(0,2));
+		bpanel.add(startButton);	//tooltip, click or press Enter to start the quiz. 
+		bpanel.add(quitButton);
+		
+		//actions
+		quitButton.addActionListener(new quitActionListener());
+		startButton.addActionListener(new startActionListener());
+		
+		return bpanel;
+	}
+	
+	//constructor
+	public AAquiz(String title)
+	{
+		super(title);
+		//sets frame to middle of the screen / alt quizFrame.setLocation(x,y);
+		setLocationRelativeTo(null);
+		setSize(500,100);
+		
+		//adding content
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(instructions, BorderLayout.NORTH);
+		getContentPane().add(getTimePanel(), BorderLayout.CENTER);
+		getContentPane().add(getBottomPanel(), BorderLayout.SOUTH);		
+		
+		updateText();
+		setVisible(true);
+		//force VM to kill frame in memory
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 	public static void main(String[] args)
 	{
-		new introWindow("Amino Acid Quiz: Welcome!");
-		new quizWindow("Amino Acid Quiz: Goodluck!");
-		new reportWindow("Amino Acid Quiz: Report");
+		new AAquiz("Amino Acid Quiz: Welcome!");
 	}
 }
---
+++ src/lab7/quizFunc.java
@@ -0,0 +1,82 @@
+package lab7;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+public class quizFunc extends JFrame
+{
+	private static final long serialVersionUID = -3485284893489L;
+	
+	private JLabel question = new JLabel();
+	private JTextField enterAnswer = new JTextField();
+	private JTextArea result = new JTextArea();
+	private JButton cancelButton = new JButton("Cancel");
+	private JLabel timer = new JLabel();
+	
+	private void updateText()
+	{
+		question.setText("long name of aminoacid here:"); //updates after result displays below
+		result.setText("Show answer to last question here"); //Display correct or incorrect with correct aa letter.
+		result.setEditable(false);
+		
+		validate();
+	}
+/*	private void updateTimer()
+	{
+		timer.setText(userTime);
+		
+		validate();
+	}
+	*/
+	
+	
+	private JPanel getTopPanel()
+	{
+		JPanel tpanel = new JPanel();
+		tpanel.setLayout(new GridLayout(0,2));
+		tpanel.add(question);
+		tpanel.add(enterAnswer);
+		
+		return tpanel;
+	}
+	private JPanel getBottomPanel()
+	{
+		JPanel bpanel = new JPanel();
+		bpanel.setLayout(new GridLayout(0,2));
+		bpanel.add(timer);
+		bpanel.add(cancelButton);
+		return bpanel;
+	}
+	
+	public quizFunc(String title)
+	{
+		super(title);
+		setLocationRelativeTo(null);
+		setSize(500,100);
+		
+		//content
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(getTopPanel(), BorderLayout.NORTH);
+		getContentPane().add(result, BorderLayout.CENTER);
+		getContentPane().add(getBottomPanel(), BorderLayout.SOUTH);
+		
+		updateText();
+//		updateTimer();
+		setVisible(true);
+		//force VM to kill frame in memory
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args)
+	{
+		new quizFunc("Amino Acid Quiz: Goodluck!");
+	}
+}
